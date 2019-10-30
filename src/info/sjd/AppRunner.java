b2eff44dd9a548fdd4ff2fbd9fe038e2f8c75522
Домрачевы <@192.168.0.13>
2019-10-30 19:57:29 +0300

    Add logger

--- src/info/sjd/AppRunner.java
+++ src/info/sjd/AppRunner.java
@@ -1,27 +1,31 @@
 package info.sjd;
 
+import java.util.logging.Logger;
+
 import info.sjd.heirs.circle.Circle;
 import info.sjd.heirs.square.Square;
 import info.sjd.heirs.triangle.Triangle;
 
 public class AppRunner {
 
+	private static Logger logger = Logger.getLogger(AppRunner.class.getName());
+
 	public static void main(String[] args) {
 		Circle circle = new Circle();
 		circle.setRadius(11);
 
-		System.out.println("Area of circle with radius" + circle.getRadius() + " is " + circle.getArea());
+		logger.info("Area of circle with radius" + circle.getRadius() + " is " + circle.getArea());
 
 		Square square = new Square();
 		square.setSide(21);
 
-		System.out.println("Area of square with side" + square.getSide() + " is " + square.getArea());
+		logger.info("Area of square with side" + square.getSide() + " is " + square.getArea());
 
 		Triangle triangle = new Triangle();
 		triangle.setBase(5);
 		triangle.setHeight(18);
 
-		System.out.println("Area of triangle with base" + triangle.getBase() + " is " + triangle.getArea());
+		logger.info("Area of triangle with base" + triangle.getBase() + " is " + triangle.getArea());
 
 	}
 
--- src/info/sjd/abstraction/AbstractShape.java
+++ src/info/sjd/abstraction/AbstractShape.java
@@ -1,9 +1,7 @@
 package info.sjd.abstraction;
 
-import info.sjd.abstraction.Shape;
-
 public abstract class AbstractShape implements Shape {
-	
+
 	public abstract double getArea();
 
 }
