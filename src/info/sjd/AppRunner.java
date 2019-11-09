4ee7b1609b0a38f4d9ad79ff0d3457a962b4433b
Домрачевы <@192.168.0.13>
2019-11-09 18:05:22 +0300

    Remove excess shapes and add first collection

--- src/info/sjd/AppRunner.java
+++ src/info/sjd/AppRunner.java
@@ -1,14 +1,8 @@
 package info.sjd;
 
 import java.util.logging.Logger;
-
 import info.sjd.heirs.circle.Circle;
-import info.sjd.heirs.elipse.Elipse;
-import info.sjd.heirs.pentagon.Pentagon;
-import info.sjd.heirs.rectangle.Rectangle;
-import info.sjd.heirs.rhombus.Rhombus;
 import info.sjd.heirs.square.Square;
-import info.sjd.heirs.trapezium.Trapezium;
 import info.sjd.heirs.triangle.Triangle;
 
 public class AppRunner {
@@ -16,52 +10,51 @@ public class AppRunner {
 	public static Logger logger = Logger.getLogger(AppRunner.class.getName());
 
 	public static void main(String[] args) {
-		Circle circle = new Circle();
-		circle.setRadius(11);
 
-		logger.info("Area of circle with radius" + circle.getRadius() + " is " + circle.getArea());
+		Circle circle1 = new Circle();
+		circle1.setRadius(11);
+
+		Circle circle2 = new Circle();
+		circle2.setRadius(29);
+
+		Circle circle3 = new Circle();
+		circle3.setRadius(47);
+
+		logger.info("Area of circle with radius" + circle1.getRadius() + " is " + circle1.getArea());
+		logger.info("Area of circle with radius" + circle2.getRadius() + " is " + circle2.getArea());
+		logger.info("Area of circle with radius" + circle3.getRadius() + " is " + circle3.getArea());
 
 		Square square = new Square();
 		square.setSide(21);
 
+		Square square62 = new Square();
+		square62.setSide(62);
+
+		Square square38 = new Square();
+		square38.setSide(38);
+
 		logger.info("Area of square with side" + square.getSide() + " is " + square.getArea());
-
-		Trapezium trapezium = new Trapezium();
-		trapezium.setBase(8);
-		trapezium.setSecondbase(14);
-		trapezium.setHeight(11);
-
-		logger.info("Area of trapezium with bases" + trapezium.getBase() + "," + trapezium.getSecondbase() + " and height " + trapezium.getHeight() + " is " + trapezium.getArea());
+		logger.info("Area of square with side" + square62.getSide() + " is " + square62.getArea());
+		logger.info("Area of square with side" + square38.getSide() + " is " + square38.getArea());
 
 		Triangle triangle = new Triangle();
-		triangle.setBase(5);
-		triangle.setHeight(18);
+		triangle.setBase(69);
+		triangle.setHeight(96);
 
-		logger.info("Area of triangle with base" + triangle.getBase() +" and height " + triangle.getHeight() + " is " + triangle.getArea());
-		
-		Rhombus rhombus = new Rhombus();
-		rhombus.setDiagonal(53);
-		rhombus.setSeconddiagonal(34);
-		
-		logger.info("Area of rhombus with diagonals" + rhombus.getDiagonal() + " and " + rhombus.getSeconddiagonal() + " is " + rhombus.getArea());
-		
-		Rectangle rectangle = new Rectangle(); 
-		rectangle.setSideI(6);
-		rectangle.setSideII(99);
-		
-		logger.info("Area of rectangle with firstside" + rectangle.getSideI() + " and secondside " + rectangle.getSideII() + " is " + rectangle.getArea());
-		
-		Elipse elipse = new Elipse();
-		elipse.setAxis(8);
-		elipse.setSecondaxis(4);
-		
-		logger.info("Area of elipse with axises" + elipse.getAxis() + " and " + elipse.getSecondaxis() + " is " + elipse.getArea());
-		
-		Pentagon pentagon = new Pentagon();
-		pentagon.setSide(3);
-		pentagon.setApothem(2);
-		
-		logger.info("Area of Pentagon with side" + pentagon.getSide() + " and apothem " + pentagon.getApothem() + " is " + pentagon.getArea());
+		Triangle triangleII = new Triangle();
+		triangleII.setBase(15);
+		triangleII.setHeight(51);
+
+		Triangle triangleIII = new Triangle();
+		triangleIII.setBase(7);
+		triangleIII.setHeight(21);
+
+		logger.info("Area of triangle with base" + triangle.getBase() + " and height " + triangle.getHeight() + " is "
+				+ triangle.getArea());
+		logger.info("Area of triangle with base" + triangleII.getBase() + " and height " + triangleII.getHeight()
+				+ " is " + triangleII.getArea());
+		logger.info("Area of triangle with base" + triangleIII.getBase() + " and height " + triangleIII.getHeight()
+				+ " is " + triangleIII.getArea());
 
 	}
 
