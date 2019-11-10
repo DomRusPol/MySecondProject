d933763466c9c14951a523c63778d020f7ee7114
Домрачевы <@192.168.0.13>
2019-11-10 20:16:10 +0300

    Add collection of shapes

--- src/info/sjd/AppRunner.java
+++ src/info/sjd/AppRunner.java
@@ -1,6 +1,9 @@
 package info.sjd;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+import info.sjd.abstraction.Shape;
 import info.sjd.heirs.circle.Circle;
 import info.sjd.heirs.square.Square;
 import info.sjd.heirs.triangle.Triangle;
@@ -20,41 +23,64 @@ public class AppRunner {
 		Circle circle3 = new Circle();
 		circle3.setRadius(47);
 
-		logger.info("Area of circle with radius" + circle1.getRadius() + " is " + circle1.getArea());
-		logger.info("Area of circle with radius" + circle2.getRadius() + " is " + circle2.getArea());
-		logger.info("Area of circle with radius" + circle3.getRadius() + " is " + circle3.getArea());
+	//	logger.info("Area of circle with radius" + circle1.getRadius() + " is " + circle1.getArea());
+	//	logger.info("Area of circle with radius" + circle2.getRadius() + " is " + circle2.getArea());
+	//	logger.info("Area of circle with radius" + circle3.getRadius() + " is " + circle3.getArea());
 
-		Square square = new Square();
-		square.setSide(21);
+		Square square1 = new Square();
+		square1.setSide(21);
 
-		Square square62 = new Square();
-		square62.setSide(62);
+		Square square2 = new Square();
+		square2.setSide(62);
 
-		Square square38 = new Square();
-		square38.setSide(38);
+		Square square3 = new Square();
+		square3.setSide(38);
 
-		logger.info("Area of square with side" + square.getSide() + " is " + square.getArea());
-		logger.info("Area of square with side" + square62.getSide() + " is " + square62.getArea());
-		logger.info("Area of square with side" + square38.getSide() + " is " + square38.getArea());
+	//	logger.info("Area of square with side" + square1.getSide() + " is " + square1.getArea());
+	//	logger.info("Area of square with side" + square2.getSide() + " is " + square2.getArea());
+	//	logger.info("Area of square with side" + square3.getSide() + " is " + square3.getArea());
 
-		Triangle triangle = new Triangle();
-		triangle.setBase(69);
-		triangle.setHeight(96);
+		Triangle triangle1 = new Triangle();
+		triangle1.setBase(690);
+		triangle1.setHeight(960);
 
-		Triangle triangleII = new Triangle();
-		triangleII.setBase(15);
-		triangleII.setHeight(51);
+		Triangle triangle2 = new Triangle();
+		triangle2.setBase(15);
+		triangle2.setHeight(51);
 
-		Triangle triangleIII = new Triangle();
-		triangleIII.setBase(7);
-		triangleIII.setHeight(21);
+		Triangle triangle3 = new Triangle();
+		triangle3.setBase(7);
+		triangle3.setHeight(21);
 
-		logger.info("Area of triangle with base" + triangle.getBase() + " and height " + triangle.getHeight() + " is "
-				+ triangle.getArea());
-		logger.info("Area of triangle with base" + triangleII.getBase() + " and height " + triangleII.getHeight()
-				+ " is " + triangleII.getArea());
-		logger.info("Area of triangle with base" + triangleIII.getBase() + " and height " + triangleIII.getHeight()
-				+ " is " + triangleIII.getArea());
+	//	logger.info("Area of triangle with base" + triangle1.getBase() + " and height " + triangle1.getHeight() + " is "
+	//			+ triangle1.getArea());
+	//	logger.info("Area of triangle with base" + triangle2.getBase() + " and height " + triangle2.getHeight() + " is "
+	//			+ triangle2.getArea());
+	//	logger.info("Area of triangle with base" + triangle3.getBase() + " and height " + triangle3.getHeight() + " is "
+	//			+ triangle3.getArea());
+
+		List<Shape> shapes = new ArrayList<>();
+
+		shapes.add(circle1);
+		shapes.add(circle2);
+		shapes.add(circle3);
+		shapes.add(square1);
+		shapes.add(square2);
+		shapes.add(square3);
+		shapes.add(triangle1);
+		shapes.add(triangle2);
+		shapes.add(triangle3);
+
+		logger.info(String.valueOf(shapes.size()));
+
+		Shape maxShape = shapes.get(0);
+		for (int i = 1; i < shapes.size(); i++) {
+			if (maxShape.getArea() < shapes.get(i).getArea()) {
+				maxShape = shapes.get(i);
+			}
+		}
+
+		logger.info(String.valueOf("Area of max shape is " + maxShape.getArea() + " and it's a " + maxShape.getClass().getSimpleName()));
 
 	}
 
